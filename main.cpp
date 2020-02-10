2cbfe73141386e3ddc89d863e1a4ce02eb7ce727
XWHQSJ <xwhqsj@gmail.com>
2020-02-10 17:48:16 +0800

    add 剑指offer 01 二维数组的查找 Find.cpp & Find.h test in main.cpp

--- main.cpp
+++ main.cpp
@@ -7,12 +7,19 @@
  * */
 
 #include <iostream>
+#include <assert.h>
+#include "Find.h"
 
 using namespace std;
 
 int main()
 {
-
+    std::vector<std::vector<int>> array = {{1,2,3,4},{2,3,4,5},{3,4,5,6},{4,5,6,7}};
+    int target = 9;
+    Solution01 solution01;
+    bool b = solution01.Find(target, array);
+    // assert(b);
+    std::cout << b << std::endl;
 
     return 0;
 }
