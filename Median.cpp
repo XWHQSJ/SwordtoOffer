4f12394ce7d54aba3717fe2ec9b0471b32473466
XWHQSJ <xwhqsj@gmail.com>
2020-02-10 20:02:38 +0800

    add 剑指offer 01 二维数组的查找 扩展题 —— 中位数 Median.cpp & Median.h && test in main.cpp

---
+++ Median.cpp
@@ -0,0 +1,64 @@
+//
+// Created by Wanhui on 2/10/20.
+//
+
+#include "Median.h"
+
+int Solution01_1::median(int n, unsigned int x, std::vector<unsigned int> arr) {
+
+    int less = 0, high = 0, equal = 0;    //分别表示array中数与x比较的个数——小，大，等
+    for(int i = 0; i < n; i++)
+    {
+        if(arr[i] == x)
+        {
+            ++equal;
+        }
+        else if(arr[i] < x)
+        {
+            ++less;
+        }
+        else
+        {
+            ++high;
+        }
+    }
+
+    int add = 0;                        //需要增加的数的个数
+
+    // 下面通过左边的数和右边的数进行抵消来判断增加的数
+    if(less == high)
+    {
+        if(equal == 0)
+        {
+            add = 1;
+        }
+        else
+        {
+            add = 0;
+        }
+    }
+    else if(less < high)
+    {
+        if((high - less) < equal)
+        {
+            add = 0;
+        }
+        else
+        {
+            add = high - less - equal;
+        }
+    }
+    else
+    {
+        if((less - high) < equal)
+        {
+            add = 0;
+        }
+        else
+        {
+            add = less - high - equal + 1;          //这里注意是+1，因为索引从0开始
+        }
+    }
+
+    return add;
+}
--- main.cpp
+++ main.cpp
@@ -9,11 +9,13 @@
 #include <iostream>
 #include <assert.h>
 #include "Find.h"
+#include "Median.h"
 
 using namespace std;
 
 int main()
 {
+    // Find.cpp test
     std::vector<std::vector<int>> array = {{1,2,3,4},{2,3,4,5},{3,4,5,6},{4,5,6,7}};
     int target = 9;
     Solution01 solution01;
@@ -21,8 +23,14 @@ int main()
     // assert(b);
     std::cout << b << std::endl;
 
+    // Median.cpp test
+    Solution01_1 solution01_1;
+    std::vector<unsigned int> arr = {2,3,4};
+    int n = arr.size();
+    int x = 2;
+    int add = solution01_1.median(n, x, arr);
+    std::cout << add << std::endl;
+
     return 0;
 }
-
-
 
