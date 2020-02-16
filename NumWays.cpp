b08d11e2037cd429df1fe685a9406b6f4956a80a
XWHQSJ <xwhqsj@gmail.com>
2020-02-16 20:25:27 +0800

    format code

--- NumWays.cpp
+++ NumWays.cpp
@@ -8,20 +8,20 @@
 
 int Solution10_2::numWays(int n) {
     int f = 0;
-    if(f > 1000000007) {
+    if (f > 1000000007) {
         f %= 1000000007;
     }
 
-    if(n == 1) {
+    if (n == 1) {
         f = 1;
     }
 
-    if(n == 2) {
+    if (n == 2) {
         f = 2;
     }
 
-    if(n > 2) {
-        f = numWays(n-2) + numWays(n-1);
+    if (n > 2) {
+        f = numWays(n - 2) + numWays(n - 1);
     }
 
     return f;
@@ -30,8 +30,8 @@ int Solution10_2::numWays(int n) {
 int Solution10_2::numWays2(int n) {
     std::vector<int> vec = {1, 1};
 
-    for(int i = 2; i <= n; i++){
-        vec.push_back((vec[i-2] + vec[i-1])%1000000007);
+    for (int i = 2; i <= n; i++) {
+        vec.push_back((vec[i - 2] + vec[i - 1]) % 1000000007);
     }
 
     return vec[n];
