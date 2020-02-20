877b7e8c14580ca6b73c0b3483b16af8856750aa
XWHQSJ <xwhqsj@gmail.com>
2020-02-20 23:27:04 +0800

    add 剑指offer 22 链表中倒数第k个节点 GetKthFromEnd.cpp && GetKthFromEnd.h

--- GetKthFromEnd.cpp
+++ GetKthFromEnd.cpp
@@ -5,7 +5,25 @@
 #include "GetKthFromEnd.h"
 
 ListNode *Solution22::getKthFromEnd(ListNode *head, int k) {
+    if (head == nullptr || k == 0) {
+        return nullptr;
+    }
 
+    ListNode *pa = head;
+    ListNode *pb = head;
 
-    return nullptr;
+    for (int i = 1; i < k; i++) {
+        if (pa->next != nullptr) {
+            pa = pa->next;
+        } else {
+            return nullptr;
+        }
+    }
+
+    while (pa->next != nullptr) {
+        pa = pa->next;
+        pb = pb->next;
+    }
+
+    return pb;
 }
