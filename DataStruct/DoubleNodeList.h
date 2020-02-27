b4637a59e696f4bbed8cb1a7c2953fc5cb97cb92
XWHQSJ <xwhqsj@gmail.com>
2020-02-27 17:37:46 +0800

    add Node.h and NodeList.h to DataStruct update3

--- DataStruct/DoubleNodeList.h
+++ DataStruct/DoubleNodeList.h
@@ -12,37 +12,65 @@
 template <typename T>
 class DoubleNodeList {
 public:
+    // 构造函数
     DoubleNodeList();
+    // 拷贝构造函数
+    DoubleNodeList(const DoubleNodeList& other);
+    // 析构函数
     ~DoubleNodeList();
 
+    // 判断链表是否为空
     bool empty() const;
+    // 获取链表节点是数量
     std::size_t size() const;
 
+    // 获得头节点
     T& front();
+    // 获得尾节点
     T& back();
+    // [] 像数组一样获得position处的节点
     T& operator[](std::size_t position);
+
+    // 获得position处的节点
     T& at(std::size_t position);
 
+    // 删除节点
     void erase(std::size_t position);
+    // 插入节点
     void insert(std::size_t position, const T& value);
 
+    // 从头节点处压入节点
     void push_front(const T& value);
+    // 从尾节点处添加节点
     void push_back(const T& value);
+    // 从头节点出弹出节点
     void pop_front();
+    // 从尾节点出弹出节点
     void pop_back();
 
+    // 清除所有节点
     void clear();
+    // 给节点值排序（升序）
     void sort();
 
-//    void remove(const T& value);
-//    void assign(const DoubleNodeList& other);
-//    void swap(const DoubleNodeList& other);
+    // 按值删除节点
+    void remove(const T& value);
+    // = 像数值一样，赋值链表
+    T& operator=(DoubleNodeList& other);
+    // 赋值链表
+    void assign(const DoubleNodeList& other);
+    // 交换链表
+    void swap(const DoubleNodeList& other);
 
 private:
+    // 找到position处的节点
     BiNode<T>* find(std::size_t position);
+    // 在first与second中间插入值为value的节点
     void insert_between(BiNode<T> *first, BiNode<T> *second, const T& value);
+    // 在first与second中间删除值为value的节点
     void remove_inner(BiNode<T> *first, BiNode<T> *second, BiNode<T> *third);
 
+    // 链表节点数量
     std::size_t mSize;
     // 头哨兵
     BiNode<T>* mHeadSentinel;
@@ -54,6 +82,19 @@ template<typename T>
 DoubleNodeList<T>::DoubleNodeList() : mSize(0), mHeadSentinel(new BiNode<T>()), mTailSentinel(new BiNode<T>()) {
     mHeadSentinel->mNetx = mTailSentinel;
     mTailSentinel->mPrev = mHeadSentinel;
+}
+
+template<typename T>
+DoubleNodeList<T>::DoubleNodeList(const DoubleNodeList &other) : mSize(0), mHeadSentinel(new BiNode<T>()), mTailSentinel(new BiNode<T>()) {
+    if(other.mHeadSentinel == nullptr){
+        return;
+    }
+
+    BiNode<T> *tmp = other.mHeadSentinel;
+    while (tmp != nullptr){
+        push_back(tmp->mData);
+        tmp = tmp->mNetx;
+    }
 }
 
 template<typename T>
@@ -239,6 +280,40 @@ DoubleNodeList<T>::~DoubleNodeList() {
 
     delete mHeadSentinel;
     mHeadSentinel = nullptr;
+}
+
+template<typename T>
+void DoubleNodeList<T>::remove(const T &value) {
+    if(mHeadSentinel == mTailSentinel){
+        return;
+    }
+
+    BiNode<T> *tmp = mHeadSentinel->mNetx;
+    while (tmp != mTailSentinel){
+        if(tmp->mData == value){
+            remove_inner(tmp->mPrev, tmp, tmp->mNetx);
+        }
+        tmp = tmp->mNetx;
+    }
+}
+
+template<typename T>
+T &DoubleNodeList<T>::operator=(DoubleNodeList &other) {
+    return assign(other);
+}
+
+template<typename T>
+void DoubleNodeList<T>::assign(const DoubleNodeList &other) {
+    if(this != &other){
+        DoubleNodeList tmp(other);
+        swap(tmp);
+    }
+}
+
+template<typename T>
+void DoubleNodeList<T>::swap(const DoubleNodeList &other) {
+    std::swap(this->mHeadSentinel, other.mHeadSentinel);
+    std::swap(this->mTailSentinel, other.mTailSentinel);
 }
 
 
