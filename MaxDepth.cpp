5f4ea25463d26aca33d4e2d230716f877235b088
XWHQSJ <xwhqsj@gmail.com>
2020-03-10 11:34:42 +0800

    add 剑指offer 55_I 二叉树的深度 MaxDepth.cpp && MaxDepth.h

---
+++ MaxDepth.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Wanhui on 3/10/20.
+//
+
+#include <algorithm>
+#include "MaxDepth.h"
+
+int Solution55_1::maxDepth(BiTreeNode *root) {
+    if(root == nullptr){
+        return 0;
+    }
+
+    // 递归左子树
+    int leftDepth = maxDepth(root->left);
+    // 递归右子树
+    int rightDepth = maxDepth(root->right);
+
+    // 返回左右子树深度较大的值加1
+    return std::max(leftDepth, rightDepth) + 1;
+
+    // 将上述三步总结为一句返回
+//    return std::max(maxDepth(root->left), maxDepth(root->right)) + 1;
+}
--- cmake-build-debug/CMakeFiles/SwordtoOffer.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/SwordtoOffer.dir/cmake_clean.cmake
@@ -19,6 +19,7 @@ file(REMOVE_RECURSE
   "CMakeFiles/SwordtoOffer.dir/IsSubStructure.cpp.o"
   "CMakeFiles/SwordtoOffer.dir/IsSymmetric.cpp.o"
   "CMakeFiles/SwordtoOffer.dir/MajorityElement.cpp.o"
+  "CMakeFiles/SwordtoOffer.dir/MaxDepth.cpp.o"
   "CMakeFiles/SwordtoOffer.dir/MaxSlidingWindow.cpp.o"
   "CMakeFiles/SwordtoOffer.dir/MaxSubArray.cpp.o"
   "CMakeFiles/SwordtoOffer.dir/Median.cpp.o"
