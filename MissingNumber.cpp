78cab3fa44e1f4ac47f74cb7d80c56cedb9a85e1
XWHQSJ <xwhqsj@gmail.com>
2020-03-21 19:24:03 +0800

    add 剑指offer 53-II 0-n-1中缺失的数字 MissingNumber.cpp && MissingNumber.h and main.cpp method1 桶排序 method2 二分查找 method3 数学规律

--- MissingNumber.cpp
+++ MissingNumber.cpp
@@ -28,17 +28,40 @@ int Solution53_2::missingNumber(std::vector<int> &nums) {
 }
 
 int Solution53_2::missingNumber2(std::vector<int> &nums) {
-    if(nums.empty()){
+    if (nums.empty()) {
         return -1;
     }
 
+    int start = 0, end = nums.size() - 1;
 
+    // 对数组进行二分查找
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
 
-    return 0;
+        // 如果中间数与索引不相等
+        if (nums[mid] != mid) {
+            // 如果中间数的前一个数与索引相等，
+            // 则中间数对应的索引为缺失数
+            if (mid == 0 || nums[mid - 1] == mid - 1) {
+                return mid;
+            }
+
+            // 如果中间数的前一个数与索引不相等，
+            // 则继续对数组左边进行二分查找
+            end = mid - 1;
+        } else {
+            // 如果中间数与索引相等，则继续对数组右边进行二分查找
+            start = mid + 1;
+        }
+    }
+
+    if (start == nums.size()) {
+        return nums.size();
+    }
 }
 
 int Solution53_2::missingNumber3(std::vector<int> &nums) {
-    if(nums.empty()){
+    if (nums.empty()) {
         return -1;
     }
 
@@ -46,11 +69,11 @@ int Solution53_2::missingNumber3(std::vector<int> &nums) {
     int n = nums.size() + 1;
 
     // 计算n个数的和
-    int sum1 = (n * (n-1))/2;
+    int sum1 = (n * (n - 1)) / 2;
 
     // 计算数组中所有数的和
     int sum2 = 0;
-    for(int num : nums){
+    for (int num : nums) {
         sum2 += num;
     }
 
