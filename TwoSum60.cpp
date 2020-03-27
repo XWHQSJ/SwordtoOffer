aea63f97a23444402ba58c4d0e8682fa3babd0a5
XWHQSJ <xwhqsj@gmail.com>
2020-03-27 21:50:33 +0800

    add 剑指offer 60 n个骰子的点数 TwoSum60.cpp

--- TwoSum60.cpp
+++ TwoSum60.cpp
@@ -5,7 +5,25 @@
 #include "TwoSum60.h"
 
 std::vector<double> Solution60::twoSum(int n) {
+    if (n < 0) {
+        return {};
+    }
 
+    std::vector<double> dp(n * 6 + 1, 0.0);
+    for (int i = 1; i <= 6; i++) {
+        dp[i] = 1.0 / 6.0;
+    }
 
-    return std::vector<double>();
+    for (int i = 2; i <= n; i++) {
+        for (int j = i * 6; j >= i; j--) {
+            dp[j] = 0;
+            for (int k = j-1; k >= i-1 && k >= j-6; k--) {
+                dp[j] += dp[k] * 1.0 / 6.0;
+            }
+        }
+    }
+
+    std::vector<double> ans(dp.begin() + n, dp.end());
+
+    return ans;
 }
